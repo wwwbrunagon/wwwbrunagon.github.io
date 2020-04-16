dc125af27be418663c34b1715a9e8541ed9daa6e
wwwbrunagon <wwwbrunagon@gmail.com>
2020-04-16 03:40:52 -0300

    completed layout desktop-version

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,5 +1,6 @@
+//tabpanel
 const tabClick = ({ target }) => {
-    const { dataset: { id = '' }} = target;
+    const { dataset: {id = '' }} = target;
     document.querySelectorAll('.tab').forEach(t => t.classList.remove('selected'));
     target.classList.add('selected');
     document.querySelectorAll('.tab-panel').forEach(t => t.classList.add('hidden'));
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-//skills
+//skills progressbar
 function getRandomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
@@ -29,18 +30,30 @@ function createProgressBar(percentage, containerDiv) {
     containerDiv.appendChild(percentage_p)  
   
     const progressBar = document.createElement("div")
- 
+     
     progressBar.style.backgroundColor = getRandomColor()
-    setTimeout(() => {
-      progressBar.style.width = percentage + "%"
-    }, 100)
-    
+    progressBar.style.width = percentage + "%"    
     containerDiv.appendChild(progressBar)
   }
   
-
-  const progress = document.querySelectorAll(".progress") 
+  const progress = document.querySelectorAll(".progressbar") 
   for (const div of progress) { 
     const percentage = div.dataset.percentage
     createProgressBar(percentage, div)
   }
+
+
+//particles
+let particles = Particles.init({
+  selector: '.background',
+  color: ['#474b4f', '#404B69'],
+  connectParticles: true,
+  responsive: [{
+    breakpoint: 800,
+    options: {
+      color: '#00C9B1',
+      maxParticles: 80,
+      connectParticles: false
+    }
+  }]
+});
